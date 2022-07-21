b0d106ed8e1f540950a09dea46a7e86ff89bd718
Andrzej Karcinski <andrzej.karcinski@gmail.com>
2022-07-21 16:06:16 +0200

    task1

--- Pytest/Task1/test_function.py
+++ Pytest/Task1/test_function.py
@@ -1,2 +1,12 @@
 from function import square
-# Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100
+import pytest
+# Napisać cztery testy z dla funckji square dla 0, 1, -1 i 100 
+class test:
+    def test_A(self):
+        assert square(0) == 0
+    def test_B(self):
+        assert square(1) == 1
+    def test_C(self):
+        assert square(-1) == 1
+    def test_D(self):
+        assert square(100) == 10000
